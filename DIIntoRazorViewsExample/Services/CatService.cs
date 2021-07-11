354240bf43e0411af375d94427e89fe55c1ba922
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-11 13:45:33 +0200

    Added new class to Services folder

---
+++ DIIntoRazorViewsExample/Services/CatService.cs
@@ -0,0 +1,12 @@
+﻿using DIIntoRazorViewsExample.Models;
+
+namespace DIIntoRazorViewsExample.Services
+{
+    public class CatService : ICatService
+    {
+        public Cat GetName()
+        {
+            return new Cat { Name = "Simon" };
+        }
+    }
+}
