82da498de9a47bbe594b767d72124fa26aeca24a
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-11 13:07:33 +0200

    Added Services folder and inteface into this folder

---
+++ DIIntoRazorViewsExample/Services/ICatService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DIIntoRazorViewsExample.Models;
+
+namespace DIIntoRazorViewsExample.Services
+{
+    public interface ICatService
+    {
+        Cat GetName();
+    }
+}
