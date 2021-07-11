fae9e9753ea92a04679febfcf4a8cb908aa1c279
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-11 13:27:47 +0200

    Removed unnecessary namespace

--- DIIntoRazorViewsExample/Services/ICatService.cs
+++ DIIntoRazorViewsExample/Services/ICatService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DIIntoRazorViewsExample.Models;
+﻿using DIIntoRazorViewsExample.Models;
 
 namespace DIIntoRazorViewsExample.Services
 {
